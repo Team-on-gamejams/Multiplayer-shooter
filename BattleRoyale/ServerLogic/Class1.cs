72dd17fbb65147049098fc9141130023faaf9b0a
Team <timonsol13@gmail.com>
2018-10-27 18:55:52 +0300

    Init solutions

---
+++ BattleRoyale/ServerLogic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLogic
+{
+    public class Class1
+    {
+    }
+}
